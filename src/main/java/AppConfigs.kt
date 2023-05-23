f39c13a2e7e5fd8b5c4dd1012d05a2fc708cecf0
Hoangkute123xyz <hoangdv@wavez.vn>
2023-05-23 10:26:41 +0700

    update

---
+++ src/main/java/AppConfigs.kt
@@ -0,0 +1,5 @@
+object AppConfigs {
+    const val APP_ID = "com.wavez.p59_smusic"
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+}
--- src/main/java/Configs.kt
+++ src/main/java/Configs.kt
@@ -1,9 +1,6 @@
 import org.gradle.api.JavaVersion
 
 object Configs {
-    const val APP_ID = "com.wavez.p59_smusic"
-    const val VERSION_CODE = 1
-    const val VERSION_NAME = "1.0"
     const val TARGET_SDK = 33
     const val MIN_SUPPORTED_SDK = 23
     val JAVA_TARGET = JavaVersion.VERSION_17
