419530c0d86b40eb960d70dca5a633741045add2
Hoangkute123xyz <hoangdv@wavez.vn>
2023-05-23 09:38:36 +0700

    initial module

---
+++ src/main/java/Plugins.kt
@@ -0,0 +1,10 @@
+object Plugins {
+    const val ANDROID_APPLICATION = "com.android.application"
+    const val ANDROID_LIBS = "com.android.library"
+    const val HILT="com.google.dagger.hilt.android"
+}
+
+object PluginsVer{
+    const val GRADLE_VER = "7.4.1"
+    const val HILT_VER = "2.44"
+}
