155a6289b1d42995ca65112635d5d90bd36d8238
Hoangkute123xyz <hoangdv@wavez.vn>
2023-06-16 16:44:44 +0700

    update

---
+++ src/main/java/AdmobDeps.kt
@@ -0,0 +1,13 @@
+object AdmobDeps {
+    const val APPLOVIN_ADAPTER = "com.google.ads.mediation:applovin:${AdmobDepsVer.APPLOVIN_VERS}"
+    const val MINTENGRAL_ADAPTER =
+        "com.google.ads.mediation:mintegral:${AdmobDepsVer.MINTEGRAL_VERS}"
+    const val IRONSOURCE_ADAPTER = "com.google.ads.mediation:ironsource:${AdmobDepsVer.IRON_SOURCE_VERS}"
+}
+
+
+object AdmobDepsVer {
+    const val APPLOVIN_VERS = "11.10.1.0"
+    const val MINTEGRAL_VERS = "16.4.51.0"
+    const val IRON_SOURCE_VERS = "7.3.1.0"
+}
